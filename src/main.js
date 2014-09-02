48b4012f8673b594765b5db85d5062b0840b8e4f
Matthew Spencer <matthew.spencer@athleticsnyc.com>
2014-09-02 11:53:39 -0400

    Update paths in main

--- src/main.js
+++ src/main.js
@@ -1,28 +1,19 @@
-var head = document.getElementsByTagName('head')[0],
-    script = document.createElement('script');
-
-script.src = 'js/vendor/requirejs/require.js';
-head.appendChild(script);
-
-
 define('jquery', function() {
-	console.log('defining jquery');
 	if ( window.jQuery ) return window.jQuery;
 
-	require('js/vendor/jquery/dist/jquery.min');
+	require('bower_components/jquery/dist/jquery.min');
 	return window.jQuery;
 });
 
 require.config({
-	baseUrl: 'js',
 	paths: {
-		'app': 'app',
-		'vendor': 'vendor'
+		'app': 'src/modules',
+		'vendor': 'bower_components'
 	}
 });
 
 requirejs([
 	'jquery',
 	'app/util',
-	'app/ad_manager/ad_manager'
+	'app/manager'
 ]);
