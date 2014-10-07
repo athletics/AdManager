02d4ddeebba953cbcf9305389023f5cfb352d446
Matthew Spencer <matthew.spencer@athleticsnyc.com>
2014-10-07 14:20:55 -0400

    Update usage

--- dist/usage.js
+++ dist/usage.js
@@ -1,20 +1,13 @@
-(function ($LAB) {
-	$LAB
-		.script( 'admanager.js' )
-		.wait(function() {
-			admanager.bootstrap.init({
-				'account' : 123456789,
-				'inventory' : [
-					{
-						'slot' : 'Unit_Name_in_DFP',
-						'type' : 'unit_data_id',
-						'iteration' : 0,
-						'sizes' : [
-							[728, 90]
-						]
-					}
-				]
-			});
-		})
-	;
-}($LAB));
+admanager.bootstrap.init({
+	'account' : 123456789,
+	'inventory' : [
+		{
+			'slot' : 'Unit_Name_in_DFP',
+			'type' : 'unit_data_id',
+			'iteration' : 0,
+			'sizes' : [
+				[728, 90]
+			]
+		}
+	]
+});
