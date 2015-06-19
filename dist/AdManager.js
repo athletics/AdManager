86c12f37281aca031d940492d64eb28a85949ded
Matthew Spencer <matthew.spencer@athleticsnyc.com>
2015-06-19 16:25:28 -0400

    Bump version to 0.5.1

--- dist/AdManager.js
+++ dist/AdManager.js
@@ -3,7 +3,7 @@
  *
  * @author Athletics - http://athleticsnyc.com
  * @see https://github.com/athletics/AdManager
- * @version 0.5.0
+ * @version 0.5.1
  *//**
  * Shared utilities for debugging and array manipulation.
  */
